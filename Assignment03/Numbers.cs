0de0bed6960cdfe98e1081e04e91c4b127f5d4d2
pdrake1988 <pdrake1@me.com>
2022-07-07 18:54:47 -0500

    did my best

---
+++ Assignment03/Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03
+{
+    internal class Numbers
+    {
+        public int[] GenerateNumbers()
+        {
+            Random random = new Random();
+            int[] ints = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                ints[i] = random.Next(100);
+            }
+            return ints;
+        }
+        public static void PrintNumbers(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public static int Fibonacci(int[] sequence, int number)
+        {
+            return Array.IndexOf(sequence, number);
+        }
+    }
+}
---
+++ Assignment03/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment03;
+//1
+Numbers randomNumbers = new Numbers();
+int[] numbers = randomNumbers.GenerateNumbers();
+Array.Reverse(numbers);
+Numbers.PrintNumbers(numbers);
+//2
+int[] fibSeq = new int[10] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(fibSeq[i]);
+}
+Console.WriteLine(Numbers.Fibonacci(fibSeq, 5));
